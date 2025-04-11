48a96b34d06c2bae04ee59eab76de0d5410ec6b6
Pedro Eduardo Medina <p.eduardomego@gmail.com>
2025-04-11 14:58:25 -0600

    Update PhoneLettersCombination.py

--- PhoneLettersCombination.py
+++ PhoneLettersCombination.py
@@ -4,6 +4,7 @@
 # is given below. Note that 1 does not map to any letters.
 
 def letter_combinations(digits):
+    import itertools
     dictPhone = {1:[''],
                  2:['a','b','c'],
                  3:['d','e','f'],
@@ -13,21 +14,34 @@ def letter_combinations(digits):
                  7:['p','q','r'],
                  8:['t','u','v'],
                  9:['w','x','y','z']}
-    lstResponse = []
-    for index, char in enumerate(digits):        
-        for letter in dictPhone[int(char)]:
-            for auxCHar in digits[index+1:] : 
-                strResponse = letter
-                for auxletter in dictPhone[int(auxCHar)]: 
-                    strResponse+=auxletter
-                lstResponse.append(auxletter)
-    return lstResponse
-                
-                    
-            
+    arrayCombinations = list()
+    for char in digits:
+        arrayCombinations+=dictPhone[int(char)]
+    Combinations = itertools.combinations(arrayCombinations,r=len(digits))
+    Combinations = [ list(lst) for lst in Combinations]
+    removeIndex = list()
+    for index, lst in enumerate(Combinations):
+        flag = False
+        listKeys = list()
+        for value in lst:
+            for key,values in dictPhone.items():
+                if value in values:
+                    listKeys.append(key)
+                    break
+        if len(listKeys) != len(set(listKeys)):
+            removeIndex.append(index)
+    Combinations = [''.join(lst) for i,lst in enumerate(Combinations) if i not in removeIndex]
+    return Combinations
             
         
+    
+    
+    
+    
+    
+    
+    
 
-#print(letter_combinations("23"), ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], "")
-print(f'{letter_combinations("532")} \n\n {["jda", "jdb", "jdc", "jea", "jeb", "jec", "jfa", "jfb", "jfc", "kda", "kdb", "kdc", "kea", "keb", "kec", "kfa", "kfb", "kfc", "lda", "ldb", "ldc", "lea", "leb", "lec", "lfa", "lfb", "lfc"]}')
-#print(letter_combinations("943"), ["wgd", "wge", "wgf", "whd", "whe", "whf", "wid", "wie", "wif", "xgd", "xge", "xgf", "xhd", "xhe", "xhf", "xid", "xie", "xif", "ygd", "yge", "ygf", "yhd", "yhe", "yhf", "yid", "yie", "yif", "zgd", "zge", "zgf", "zhd", "zhe", "zhf", "zid", "zie", "zif"], "")
+print(letter_combinations("23")== ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"], "")
+print(f'{letter_combinations("532") == ["jda", "jdb", "jdc", "jea", "jeb", "jec", "jfa", "jfb", "jfc", "kda", "kdb", "kdc", "kea", "keb", "kec", "kfa", "kfb", "kfc", "lda", "ldb", "ldc", "lea", "leb", "lec", "lfa", "lfb", "lfc"]}')
+print(letter_combinations("943") == ["wgd", "wge", "wgf", "whd", "whe", "whf", "wid", "wie", "wif", "xgd", "xge", "xgf", "xhd", "xhe", "xhf", "xid", "xie", "xif", "ygd", "yge", "ygf", "yhd", "yhe", "yhf", "yid", "yie", "yif", "zgd", "zge", "zgf", "zhd", "zhe", "zhf", "zid", "zie", "zif"], "")
