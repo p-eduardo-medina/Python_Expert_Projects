fb2a91bb4b1e025ac4ed39a2880d2ad2a93a51ee
Pedro Eduardo Medina <p.eduardomego@gmail.com>
2025-04-14 01:16:40 -0600

    Create DetectZones.py

---
+++ DetectZones.py
@@ -0,0 +1,43 @@
+def num_regions(grid):
+    import numpy as np
+    grid = np.array(grid, dtype=np.int32)
+    dictOfRegions = dict()
+    count = 0
+    for  row in range(len(grid)):
+        for column in range(len(grid[0])):
+            if grid[row][column] == 1:
+                NotinSet = True
+                for key, Coords in dictOfRegions.items():
+                    flag = False
+                    for Coord in Coords:
+                        if abs(Coord[0]-row) <= 1 ^ abs(Coord[1]-column)<=1:
+                            dictOfRegions[key].append([row,column])
+                            flag = True
+                            NotinSet = False
+                            break
+                if NotinSet:
+                    dictOfRegions[count] =[[row,column]]
+                    count+=1
+            if not bool(dictOfRegions): 
+                dictOfRegions[count] =[[row,column]]
+                count+=1
+    return len(dictOfRegions)
+
+def XOR(A, B):
+	return A ^ B
+ 
+print(num_regions([
+    [1, 1, 1, 1, 0],
+    [1, 1, 0, 1, 0],
+    [1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0]]), 1)
+print(num_regions([
+    [1, 1, 1, 1, 0],
+    [1, 0, 0, 1, 0],
+    [1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 1]]), 2)
+print(num_regions([
+    [1, 1, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1]]), 3)
