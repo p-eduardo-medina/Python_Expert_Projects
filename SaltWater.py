397820d419bd5940a78560ed993c3356769917f5
Pedro Eduardo Medina <p.eduardomego@gmail.com>
2025-04-25 13:08:38 -0600

    Update SaltWater.py

---
+++ SaltWater.py
@@ -0,0 +1,9 @@
+# A tank has pure water flowing into it at 10 l/min.
+# The contents of the tank are kept thoroughly mixed, 
+# and the contents flow out at 10 l/min. Salt is added 
+# to the tank at a rate of 0.1 kg/min. Initially, 
+# the tank contains 10 kg of salt in 100 l of water.
+# Devise a function whose argument is time t. 
+# The function returns the amount of salt (kg) 
+# left in the tank after t minutes rounded to 3 decimal places.
+
