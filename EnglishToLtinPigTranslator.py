15cc18bb0c68e2f0f80292e21a43d7c2ff7efa4c
Pedro Eduardo Medina <p.eduardomego@gmail.com>
2025-05-06 23:11:51 -0600

    Update EnglishToLtinPigTranslator.py

---
+++ EnglishToLtinPigTranslator.py
@@ -0,0 +1,22 @@
+#Some changes in words in function whether the first letter is a vogel or consonant.
+def translate_word(sentence):
+    pass
+
+
+print(translate_word("have"), "avehay")
+print(translate_word("cram"), "amcray")
+print(translate_word("take"), "aketay")
+print(translate_word("Cat"), "Atcay")
+print(translate_word("Shrimp"), "Impshray")
+print(translate_word("trebuchet"), "ebuchettray")
+print(translate_word("ate"), "ateyay")
+print(translate_word("Apple"), "Appleyay")
+print(translate_word("oaken"), "oakenyay")
+print(translate_word("eagle"), "eagleyay")
+print(translate_word("ink"), "inkyay")
+print(translate_word("unicorn"), "unicornyay")
+print(translate_word(""), "")
+print(translate_sentence("I like to eat honey waffles"), "Iyay ikelay otay eatyay oneyhay afflesway")
+print(translate_sentence("Do you think it is going to rain today?"), "Oday ouyay inkthay ityay isyay oinggay otay ainray odaytay?")
+print(translate_sentence('He said, "When will this all end?"'), 'Ehay aidsay, "Enwhay illway isthay allyay endyay?"')
+print(translate_sentence(""), "")
